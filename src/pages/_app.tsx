500bdc3bbb82ad1160d37f180701d703a3e20a2c
Ilaria <82510639+ilariiiiia@users.noreply.github.com>
2023-07-19 15:43:39 +0200

    added favicon files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <link rel="manifest" href="/site.webmanifest"/>
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+    <meta name="msapplication-TileColor" content="#00aba9"/>
+    <meta name="theme-color" content="#ffffff"/>
+  </Head>
   return <Component {...pageProps} />;
 };
 
